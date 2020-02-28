94a976a8fd5faddd4db8cb2a0893aa8bb6f3fe69
XenHat <commits@xenh.at>
2020-02-28 17:30:11 -0500

    Fix build.sh again.

--- build.bash
+++ build.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 shopt -s globstar
 # see http://astyle.sourceforge.net/astyle.html#_General_Information
-style="--style=otbs --indent=spaces=2 --convert-tabs --max-code-length=80 \
---delete-empty-lines --indent-switches --indent-preproc-block --unpad-paren \
---indent-preproc-define --pad-oper --close-templates break-blocks=all"
+style="--style=otbs --max-code-length=80 --indent=spaces=2 --convert-tabs  \
+	--delete-empty-lines --indent-switches --indent-preproc-block \
+	--unpad-paren --indent-preproc-define --pad-oper --close-templates \
+	--break-blocks=all"
 # --align-method-colon --pad-method-colon=all
 for i in **/*.lsl; do # Whitespace-safe and recursive
 	echo "> $i"
@@ -12,6 +13,8 @@ for i in **/*.lsl; do # Whitespace-safe and recursive
 		mcpp -P "$i" compiled/xenhat.kemono.body.lsl
 		sed -i '/^$/d' compiled/xenhat.kemono.body.lsl
 		astyle $style compiled/xenhat.kemono.body.lsl
+	else 
+		astyle $style "$i"
 	fi
 done
 # if [ "$i" = "compiled/xenhat.kemono.body.lsl" ]; then
