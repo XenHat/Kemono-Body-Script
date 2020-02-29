2076ec643397472c71bca27ee6bb566e01bced28
XenHat <commits@xenh.at>
2020-02-28 19:04:59 -0500

    unbreak command definitions

--- build.bash
+++ build.bash
@@ -3,8 +3,9 @@ shopt -s globstar
 # see http://astyle.sourceforge.net/astyle.html#_General_Information
 style="--style=otbs --max-code-length=80 --indent=spaces=2 --convert-tabs  \
 	--delete-empty-lines --indent-switches --indent-preproc-block \
-	--unpad-paren --indent-preproc-define --pad-oper --close-templates \
-	--break-blocks=all --indent-col1-comments"
+	--unpad-paren --pad-oper --close-templates \
+	--break-blocks=all --indent-col1-comments --align-method-colon \
+	--pad-method-colon=all --close-templates"
 # --align-method-colon --pad-method-colon=all
 for i in **/*.lsl; do # Whitespace-safe and recursive
 	if [ "$i" = "Kemono-Body-Script.lsl" ]; then
